506587c08b2b5425f4adad5ab9fd708d7b97402c
mzhan <minshuzhan@gmail.com>
2014-04-15 22:34:56 +0100

    rename

---
+++ contents/schedule.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+   var $calendar = $('#calendar');
+   var id = 10;
+
+   $calendar.weekCalendar({
+      timeslotsPerHour : 4,
+      allowCalEventOverlap : true,
+      overlapEventsSeparate: true,
+      businessHours :{start: 8, end: 18, limitDisplay: true },
+      daysToShow : 5,
+      height : function($calendar) {
+         return $(window).height() ;
+      },
+	  width : function($calendar) {
+         return $(window).width() ;
+      },
+      eventRender : function(calEvent, $event) {
+         if (calEvent.end.getTime() < new Date().getTime()) {
+            $event.css("backgroundColor", "#aaa");
+            $event.children(".wc-time").css({
+               "backgroundColor" : "#999",
+               "border" : "1px solid #888"
+            });
+         };
+		 $event.children(".wc-title").css({
+               "display" : "none"
+			   });
+      },
+      draggable : function(calEvent, $event) {
+         return calEvent.readOnly != true;
+      },
+      resizable : function(calEvent, $event) {
+         return calEvent.readOnly != true;
+      },
+      eventNew : function(calEvent, $event) {
+      },
+      eventDrop : function(calEvent, $event) {
+      },
+      eventResize : function(calEvent, calEventOld, $event) {
+      },
+      eventClick : function(calEvent, $event) {
+      },
+      eventMouseover : function(calEvent, $event) {
+      },
+      eventMouseout : function(calEvent, $event) {
+      },
+      noEvents : function() {
+      }
+   });
+
+
+});
+
+function addInterviewer(name){
+	var intrElem = document.createElement('li');
+	intrElem.textContent = name;
+	var delBtn = document.createElement('button');
+	
+	$('#intrList').appendChild()
+	}

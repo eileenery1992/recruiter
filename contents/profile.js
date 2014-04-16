4ca8444cab1952c2731561b65e98a35eca380847
Siyao Xu <eileenery28@gmail.com>
2014-04-15 23:38:51 -0400

    More building blocks.

--- contents/profile.js
+++ contents/profile.js
@@ -2,7 +2,6 @@ var selected = "tabCandidate";
 var candidate = "Ben";
 
 function updateTabs(){
-  console.log("mewww"); 
   $(".tabControl").css("background-color", "#e9eaed").css("z-index", 1);
   $(".tabPage").css("display", "none").css("z-index", 1);
   if (selected.length != 0) {
@@ -17,10 +16,6 @@ function updateTabs(){
       $("#tabProfilePage").css("display", "none").css("z-index", 2);
     }
   }   
-}
-
-function mew() {
-  console.log("mew");
 }
 
 var addAction = function(actor, type, content) {
@@ -54,31 +49,55 @@ var addAction = function(actor, type, content) {
   $("#activityList").append(activity);
 }
 
-$(document).ready(function() {
+var addAction2 = function(actor, type, content) {
+  var activity = document.createElement("div");
+  var profileLink = document.createElement("span");
+  profileLink.innerHTML = "laura";
+  profileLink.className = "profileLink";
+  var timeStamp = document.createElement("span");
+  timeStamp.innerHTML = new Date().toLocaleString();
+  timeStamp.className = "timeStamp";
+  var comment = document.createElement("span");
+  var activityClass = "";
+  if (type == "create") {
+    comment.innerHTML = " created application: 10023";
+    activityClass = "statusChange";
+  } else if (type == "review") {
+    comment.innerHTML = " added reviewer: ";
+    var reviewer = document.createElement("span");
+    reviewer.innerHTML = content;
+    reviewer.className = "profileLink";
+    comment.appendChild(reviewer);
+    activityClass = "regular";
+  } else if (type == "comment") {
+    comment.innerHTML = " commented: " + content;
+    activityClass = "comment";
+  }
+  activity.className = "activity " + activityClass;
+  activity.appendChild(profileLink);
+  activity.appendChild(comment);
+  activity.appendChild(timeStamp);
+  $("#activityList2").append(activity);
+}
+
+
+var benLoaded = 0;
+
+var loadBen = function() {
 
   updateTabs();
-  mew();
 
-  addAction("laura", "create", "");
-
-  $(".tabControl").hover(function() {
-    if (this.id != selected) {
-      this.style.backgroundColor = "#f6f7f8";
-    }
-  }, function() {
-    if (this.id != selected) {
-      this.style.backgroundColor = "#e9eaed";
-    }
-  }).click(function() {
-    selected = this.id;
-    updateTabs();
-  });
+  if (benLoaded == 0) {
+    addAction("laura", "create", "");
+    addAction2("laura", "create", "");
+    benLoaded = 1;
+  }
 
   $("#actionInput").keyup(function() {
     if ($(this).val().length != 0) {
       $("#confirmButton").css("backgroundColor", "#388ac1");
     } else {
-      $("#confirmButton").css("backgroundColor", "gray");
+      $("#confirmButton").css("backgroundColor", "gay");
     }
   });
 
@@ -139,9 +158,8 @@ $(document).ready(function() {
     this.style.textDecoration = "none";
   }).click(function() {
     candidate = "Alex";
-    console.log("before");
     updateTabs();
-    console.log("after");
+    loadAlex();
   });
 
   $("#cancelEmailButton").click(function() {
@@ -161,5 +179,99 @@ $(document).ready(function() {
       $("#reviewerInputContainer").css("display", "inline-block");
     }
   });
+};
 
+var loadAlex = function() {
+
+  $("#actionInput2").keyup(function() {
+    if ($(this).val().length != 0) {
+      $("#confirmButton2").css("backgroundColor", "#388ac1");
+    } else {
+      $("#confirmButton2").css("backgroundColor", "gray");
+    }
+  });
+
+  $("#confirmButton2").click(function() {
+    var option = $("#mySelect2").find(":selected").text();
+    if (option == "Add Comment") {
+      var comment = $("#actionInput2").val();
+      $("#actionInput2").val("");
+      if (comment.length != 0) {
+        addAction2("laura", "comment", comment);
+      }
+    } else {
+      var reviewer = $("#reviewerInput2").val();
+      if (reviewer == "mclean") {
+        addAction2("laura", "review", reviewer);
+      }
+    }
+  });
+
+  $(".profileTab.unselected, .profileTab.selected").hover(function() {
+    $(this).addClass("hovering");
+  }, function(){
+    $(this).removeClass("hovering");
+  }).click(function() {
+    $(".profileTab.selected").removeClass("selected").addClass("unselected");
+    this.className = "profileTab selected";
+  });
+
+  $('.default-value').each(function() {
+    var default_value = this.value;
+    $(this).focus(function(){
+      if(this.value == default_value) {
+      this.value = '';}
+    });
+    $(this).blur(function(){
+      if(this.value == '') {
+      this.value = default_value;}
+    });});
+  
+  $("#editButton2").hover(function() {
+    this.style.textDecoration = "underline";
+  }, function() {
+    this.style.textDecoration = "none";
+  });
+
+  $("#email2").hover(function() {
+    this.style.textDecoration = "underline";
+  }, function() {
+    this.style.textDecoration = "none";
+  }).click(function() {
+    $("#newEmail").modal("show");
+  });
+
+  $("#cancelEmailButton2").click(function() {
+    $("#newEmail").modal("hide");
+  });
+
+  $("#sendEmailButton2").click(function() {
+    $("#newEmail").modal("hide");
+  });
+
+  $("#mySelect2").click(function() {
+    if (this.selectedIndex == 0) {
+      $("#actionInputContainer2").css("display", "block");
+      $("#reviewerInputContainer2").css("display", "none");
+    } else {
+      $("#actionInputContainer2").css("display", "none");
+      $("#reviewerInputContainer2").css("display", "inline-block");
+    }
+  });
+};
+
+$(document).ready(function() {
+  $(".tabControl").hover(function() {
+    if (this.id != selected) {
+      this.style.backgroundColor = "#f6f7f8";
+    }
+  }, function() {
+    if (this.id != selected) {
+      this.style.backgroundColor = "#e9eaed";
+    }
+  }).click(function() {
+    selected = this.id;
+    updateTabs();
+  });
+  loadBen();
 });

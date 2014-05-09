c1e0d82811ddbde82d942be270266d54300da53e
Minshu Zhan <mzhan@mit.edu>
2014-05-09 01:35:29 -0400

    add missing file

---
+++ contents/update_task.php
@@ -0,0 +1,24 @@
+<?php
+$username = "mzhan";
+$password = "wxz6813";
+$hostname = "sql.mit.edu";
+$database = "mzhan+recruiter";
+
+$con=mysqli_connect($hostname, $username, $password, $database);
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$CID = $_POST['CID'];
+$action = $_POST['action'];
+$delete = $_POST['delete'];
+echo $delete;
+if ($delete){
+  $s = "DELETE FROM Tasks WHERE TaskID=$taskID ";
+} else{
+$s = "INSERT INTO Tasks (CID, Action) VALUES ('$CID', '$action')";}
+mysqli_query($con, $s) or die(mysqli_error($con));
+
+mysqli_close($con);
+?>
+
